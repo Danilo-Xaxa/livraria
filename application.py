d435efbe44be31c8cc0b037f09d6601e027aa255
Danilo Xaxa <daniloxaxa01@gmail.com>
2021-06-26 10:29:36 -0300

    formulario

--- application.py
+++ application.py
@@ -1,7 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 meu_app = Flask(__name__)
 
 @meu_app.route('/')
 def index():
-    return 'ola mundo!'
+    return render_template('index.html')
+
+
+@meu_app.route('/cumprimento/')
+def cumprimento():
+    return render_template('cumprimento.html', nome=request.args.get('nome', 'mundo'))
