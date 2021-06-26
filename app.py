83959f429adffcbb2d90f653b23190f4e4118dfe
Danilo Xaxa <daniloxaxa01@gmail.com>
2021-06-26 19:35:08 -0300

    outras pequenas mudanças

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template, request
 
+
 meu_app = Flask(__name__)
 
 @meu_app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'GET':
         return render_template('index.html')
+        
     elif request.method == 'POST':
         return render_template('cumprimento.html', nome=request.form.get('nome', 'mundo'))
