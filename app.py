4b9343ef06c15067a7cd889fd4ede35f241ed50d
Danilo Xaxa <daniloxaxa01@gmail.com>
2021-06-27 18:09:27 -0300

    table pessoas

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request
 from cs50 import *
 
 
+pessoas = {}
+
 meu_app = Flask(__name__)
 
 @meu_app.route('/', methods=['GET', 'POST'])
@@ -16,4 +18,12 @@ def index():
         elif not request.form.get('humor'):
             return render_template('erro.html', texto_erro='Você não tem emoções?')
 
-        return render_template('cumprimento.html', nome=request.form.get('nome'), humor=request.form.get('humor'))
+        nome = request.form.get('nome').strip()
+        humor = request.form.get('humor')
+        pessoas[nome] = humor
+
+        return render_template('cumprimento.html', nome=request.form.get('nome'), humor=request.form.get('humor'), pessoas=pessoas)
+
+@meu_app.route('/pessoas/')
+def route_pessoas():
+    return render_template('pessoas.html', pessoas=pessoas)
