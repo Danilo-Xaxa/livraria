d1e02fb66b24f1fe574a19d1ce5cbf6d993c5eaf
Danilo Xaxa <daniloxaxa01@gmail.com>
2021-06-28 22:14:30 -0300

    cadastrar&entrar quase feito

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from cs50 import SQL
 from smtplib import SMTP
-import teste  # deletar saporra dps das env var
-# from os import environ
-# from unidecode import unidecode (usar se o e-mail nao suportar utf-8 ou unicode)
+import teste  # TODO: Deletar saporra dps das env var
+import os
 
 
 meu_app = Flask(__name__)
@@ -13,41 +12,82 @@ db = SQL("sqlite:///pessoas.db")
 
 @meu_app.route('/', methods=['GET', 'POST'])
 def index():
-    if request.method == 'GET':
+    if request.method == "GET":
         return render_template('index.html')
 
-    elif request.method == 'POST':
-        if not request.form.get('nome').strip():
+    elif request.method == "POST":
+        if request.form.get('cadastrar'):
+            return redirect('/cadastrar/')
+        elif request.form.get('entrar'):
+            return redirect('/entrar/')
+        else: 
+            return render_template('erro.html', msg_erro='Deu bosta')
+
+    else:
+        return render_template('erro.html', msg_erro='Deu merda')
+
+
+@meu_app.route('/cadastrar/', methods=['GET', 'POST'])
+def route_cadastrar():
+    if not request.form.get('nome'):
             return render_template('erro.html', msg_erro='Você não tem nome?')
 
-        elif not request.form.get('humor'):
-            return render_template('erro.html', msg_erro='Você não tem emoções?')
+    elif not request.form.get('email'):
+        return render_template('erro.html', msg_erro='Você não tem e-mail?')
 
-        elif not request.form.get('email'):
-            return render_template('erro.html', msg_erro='Você não tem e-mail?')
+    elif not request.form.get('senha1') or not request.form.get('senha2'):
+        return render_template('erro.html', msg_erro='Preencha sua senha duas vezes.')
 
-        nome = request.form.get('nome').strip().title()
-        humor = request.form.get('humor')
-        email = request.form.get('email')
+    elif request.form.get('senha1') != request.form.get('senha2'):
+        return render_template('erro.html', msg_erro='As senhas não batem.')
 
-        db.execute("INSERT INTO registrados (nome, humor, email) VALUES(?, ?, ?)", nome, humor, email)
+    nome = request.form.get('nome').strip().title()
+    email = request.form.get('email').lower()
+    senha = request.form.get('senha1')
 
-        texto = f"Tu foi registrado, {nome}. Mais um motivo para ficar {humor}!"
-        assunto = "Oi!"
-        msg = (f"Subject: {assunto}\n\n{texto}")
-        
-        EMAIL_REMETENTE = teste.EMAIL_REMETENTE
-        EMAIL_SENHA = teste.EMAIL_SENHA
-        
-        servidor = SMTP("smtp.gmail.com", 587)
-        servidor.starttls()
-        servidor.login(EMAIL_REMETENTE, EMAIL_SENHA)
-        servidor.sendmail(EMAIL_REMETENTE, email, msg)
+    db.execute("INSERT INTO registrados (nome, email, senha) VALUES(?, ?, ?)", nome, email, senha)
 
-        return render_template('cumprimento.html', nome=nome, humor=humor)
+    texto = f"Parabéns! Tu foi registrado com sucesso, {nome}!!!"
+    assunto = "Registrado!"
+    msg_email = (f"Subject: {assunto}\n\n{texto}")
+    
+    EMAIL_REMETENTE = teste.EMAIL_REMETENTE  # TODO: os.getenv('EMAIL_REMETENTE')
+    EMAIL_SENHA = teste.EMAIL_SENHA  # TODO: os.getenv('EMAIL_SENHA')
+
+    servidor = SMTP("smtp.gmail.com", 587)
+    servidor.starttls()
+    servidor.login(EMAIL_REMETENTE, EMAIL_SENHA)
+    servidor.sendmail(EMAIL_REMETENTE, email, msg_email)
+
+    return redirect('/pessoas/')
+
+
+@meu_app.route('/entrar/', methods=['GET', 'POST'])
+def route_entrar():
+    email = request.form.get('email')
+    senha = request.form.get('senha1')
+
+    emails_registrados = []
+    senhas_registradas = []
+
+    for linha in db.execute("SELECT email, senha FROM registrados"):
+        emails_registrados.append(linha.email)
+        senhas_registradas.append(linha.senha1)
+
+    if email not in emails_registrados:
+        msg_erro = 'E-mail incorreto! Tente novamente.'
+        return render_template('erro.html', msg_erro=msg_erro)
+    elif senha not in senhas_registradas:
+        msg_erro = 'Senha incorreta! Tente novamente.'
+        return render_template('erro.html', msg_erro=msg_erro)
+
+    return redirect('/pessoas/')
 
 
 @meu_app.route('/pessoas/')
 def route_pessoas():
-    linhas = db.execute("SELECT * FROM registrados")
-    return render_template('pessoas.html', pessoas=linhas)
+    msg = 'Parabéns! Você se cadastrou ou entrou com sucesso! Essas são todas as pessoas cadastradas:'  # TODO: Personalizar mais a msg
+
+    linhas = db.execute("SELECT nome, email FROM registrados")
+
+    return render_template('pessoas.html', pessoas=linhas, msg=msg)
