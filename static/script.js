d06f31d77b2e36d2f88a416d921946f34cde797c
Danilo Xaxa <daniloxaxa01@gmail.com>
2021-06-29 23:41:32 -0300

    gambiarrex das cores

--- static/script.js
+++ static/script.js
@@ -6,37 +6,40 @@ function horarioFavicon() {
     hora = String(hora).length == 1? '0' + hora : hora
     minuto = String(minuto).length == 1? '0' + minuto : minuto
 
-    hora = 19
+    hora = 23
     minuto = 00
 
     let meuFavicon
     let msgBomHorario
+    let corA
+    let corH1
+    let bomHorario = document.getElementById('bomHorario')
+    let tagsA = document.getElementsByTagName('a')
+    let h1 = Array.from(document.getElementsByTagName('h1'))[0]
 
     if (hora > 0 && hora < 5) {
-        corBomHorario = 'white'
+        corH1 = 'white'
         document.body.style.background = 'black'
         meuFavicon = '../static/favicons/favicon-madrugada.ico'
         msgBomHorario = 'Boa madrugada!'
+        corA = 'pink'
     } else if (hora < 12) {
         document.body.style.background = 'rgb(112, 168, 187)'
         meuFavicon = '../static/favicons/favicon-manha.ico'
         msgBomHorario = 'Bom dia!'
+        corA = 'red'
     } else if (hora < 18) {
         document.body.style.background = 'orange'
         meuFavicon = '../static/favicons/favicon-tarde.ico'
         msgBomHorario = 'Boa tarde!'
+        corA = 'brown'
     } else if (hora <= 24) {
-        corBomHorario = 'white'
+        corH1 = 'white'
         document.body.style.background = 'purple'
         meuFavicon = '../static/favicons/favicon-noite.ico'
         msgBomHorario = 'Boa noite!'
+        corA = 'yellow'
     }
-
-    try {
-        let bomHorario = document.getElementById('bomHorario')
-        bomHorario.innerText = `${msgBomHorario}`
-        bomHorario.style.color = `${corBomHorario}` || 'black'
-    } catch(e) {}
 
     let link = document.querySelector("link[rel*='icon']") || document.createElement('link');
     link.type = 'image/x-icon';
@@ -45,6 +48,20 @@ function horarioFavicon() {
     head = document.getElementsByTagName('head')[0]
     head.appendChild(link);
 
+    /* GAMBIARREX DAS CORES */
+    try {
+        h1.style.color = `${corH1}` || 'black'
+    } finally {
+        try {
+            for (let tagA of Array.from(tagsA)) {
+                tagA.style.color = corA
+            }
+        } finally {
+            try {
+                bomHorario.innerText = `${msgBomHorario}`
+            } catch(e) {}
+        }
+    }
 }
 
 horarioFavicon()
