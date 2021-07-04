2482b25e78921c249a00c68640b1c443ac0615fa
Danilo Xaxa <daniloxaxa01@gmail.com>
2021-07-03 22:53:27 -0300

    pequenas refatoradas

--- limpar_db.py
+++
@@ -1,6 +0,0 @@
-from cs50 import SQL
-
-
-db = SQL("sqlite:///pessoas.db")
-
-db.execute("DELETE FROM registrados;")
--- limpar_session.py
+++
@@ -1,5 +0,0 @@
-from os import listdir, remove
-
-
-for file in listdir("./flask_session"):
-    remove(f"./flask_session/{file}")
