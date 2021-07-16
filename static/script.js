8ed134df7dd0a5f58643866460157efd263394f3
Danilo Xaxa <daniloxaxa01@gmail.com>
2021-07-16 15:03:28 -0300

    função principal js refatorada

--- static/script.js
+++ static/script.js
@@ -1,116 +1,122 @@
-(function horarioFaviconCores() {
-    // ARRUMAR ESSA GAMBIARRA MONSTRUOSA QUE É ESSA IIFE
+// Determina do turno, da paleta de cores e do favicon de cada turno
+(function principal() {
+    // Descobre a hora e o minuto atual
     let agora = new Date()
     let hora = agora.getHours()
     let minuto = agora.getMinutes()
 
-    hora = hora // pra manipular a hora manualmente (e o minuto abaixo)
+    // Manipula a hora e minuto manualmente
+    hora = hora
     minuto = minuto
 
+    // Formata bem o horário (nada de 14:0 ou 8:37)
     hora = String(hora).length == 1? '0' + hora : hora
     minuto = String(minuto).length == 1? '0' + minuto : minuto
 
-    let meuFavicon
+    // Declara variáveis com base no turno (que ainda não foi determinado)
+    let turno
+    let msgBomTurno
+    let h1BomTurno = document.getElementById('bomTurno')
 
-    let msgBomHorario
-    let h1BomHorario = document.getElementById('bomHorario')
-
+    // Declara e inicializa variáveis que são HTMLCollection de elementos
     let tagsH1 = document.getElementsByTagName('h1')
     let tagsH2 = document.getElementsByTagName('h2')
     let tagsP = document.getElementsByTagName('p')
-
     let tagsTr = document.getElementsByTagName('tr')
     let tagsA = document.getElementsByTagName('a')
     let botoes = document.getElementsByTagName('label')
 
+    // Declara variáveis que serão as cores dos elementos
     let corTexto
-    
     let corTr
     let corA
     let corBotao
-    
+    let corBotaoTd
 
+    // Determina as paletas com base no turno (que também será determinado agora)
     if (hora >= 0 && hora < 5) {
-        meuFavicon = '../static/favicons/favicon-madrugada.ico'
-        msgBomHorario = 'Boa madrugada!'
-        document.body.style.background = '#000'
+        turno = 'madrugada'
+        corFundo = '#000'
         corTexto = '#EFF9F0'
         corTr = '#09814A'
         corA = '#DDC8C4'
         corBotao = '#8C1C13'
-        document.documentElement.style.setProperty('--corTdButton', corTr);
+        corBotaoTd = corTr
     } else if (hora < 12) {
-        meuFavicon = '../static/favicons/favicon-manha.ico'
-        msgBomHorario = 'Bom dia!'
-        document.body.style.background = '#70A8BB'
+        turno = 'manha'
+        corFundo = '#70A8BB'
         corTexto = '#37323E'
         corTr = '#679436'
         corA = '#C03221'
         corBotao = '#EF3E36'
-        document.documentElement.style.setProperty('--corTdButton', corA);
+        corBotaoTd = corA
     } else if (hora < 18) {
-        meuFavicon = '../static/favicons/favicon-tarde.ico'
-        msgBomHorario = 'Boa tarde!'
-        document.body.style.background = '#FFA500'
+        turno = 'tarde'
+        corFundo = '#FFA500'
         corTexto = '#121619'
         corTr = '#14591D'
         corA = '#DD1C1A'
         corBotao = '#B9314F'
-        document.documentElement.style.setProperty('--corTdButton', corA);
+        corBotaoTd = corA
     } else if (hora <= 23) {
-        meuFavicon = '../static/favicons/favicon-noite.ico'
-        msgBomHorario = 'Boa noite!'
-        document.body.style.background = '#800080'
+        turno = 'noite'
+        corFundo = '#800080'
         corTexto = '#F2EDEB'
         corTr = '#31572C'
         corA = '#FABC2A'
         corBotao = '#BD93BD'
-        document.documentElement.style.setProperty('--corTdButton', corTr);
+        corBotaoTd = corTr
     }
 
+    // Configura a mensagem do bom turno e a cor de fundo
+    msgBomTurno = (hora >= 5 && hora < 12) ? 'Bom dia!' : `Boa ${turno}!`
+    document.body.style.background = corFundo
 
+    // Cria variáveis CSS de cores
+    document.documentElement.style.setProperty('--corBotaoTd', corBotaoTd);
     document.documentElement.style.setProperty('--botaoSelecionado', corTr);
     document.documentElement.style.setProperty('--tabela', corTr);
 
-
+    // Configura o favicon adequado para o turno
     let link = document.querySelector("link[rel*='icon']") || document.createElement('link');
     link.type = 'image/x-icon';
     link.rel = 'shortcut icon';
-    link.href = meuFavicon;
+    link.href = `../static/favicons/favicon-${turno}.ico`
     head = document.getElementsByTagName('head')[0]
     head.appendChild(link);
 
-
+    // Determina cor de cada tag (não consegui juntar os HTMLCollection num array)
     if (tagsH1) {
-        for (let tagH1 of Array.from(tagsH1)) {
-            tagH1.style.color = corTexto
+        for (let tag of Array.from(tagsH1)) {
+            tag.style.color = corTexto
         }
     }
 
     if (tagsH2) {
-        for (let tagH2 of Array.from(tagsH2)) {
-            tagH2.style.color = corTexto
+        for (let tag of Array.from(tagsH2)) {
+            tag.style.color = corTexto
         }
     }
 
     if (tagsP) {
-        for (let tagP of Array.from(tagsP)) {
-            tagP.style.color = corTexto
+        for (let tag of Array.from(tagsP)) {
+            tag.style.color = corTexto
         }
     }
 
     if (tagsTr) {
-        for (let tagTr of Array.from(tagsTr)) {
-            tagTr.style.backgroundColor = corTr
+        for (let tag of Array.from(tagsTr)) {
+            tag.style.backgroundColor = corTr
         }
     }
 
     if (tagsA) {
-        for (let tagA of Array.from(tagsA)) {
-            tagA.style.color = corA
+        for (let tag of Array.from(tagsA)) {
+            tag.style.color = corA
         }
     }
 
+    // Configura a cor de fundo e borda dos botões
     if (botoes) {
         for (let botao of Array.from(botoes)) {
             botao.style.backgroundColor = corBotao
@@ -118,20 +124,20 @@
         }
     }
 
-    if (h1BomHorario) {
-        h1BomHorario.innerText = msgBomHorario
+    // Determina a mensagem do bom turno
+    if (h1BomTurno) {
+        h1BomTurno.innerText = msgBomTurno
     }
 })()
 
 
-
+// Remove determinado livro do carrinho, chamada ao clicar o botão "Remover"
 function removerLivro(livro) {
     window.location.href=`/carrinho?removido=${encodeURI(livro)}`
 }
 
 
-
-// PARTE DO """AJAX""":
+// Simplificar isso?
 function removeAcento(text) {       
     text = text.toLowerCase()                                             
     text = text.replace(new RegExp('[ÁÀÂÃ]','gi'), 'a')
@@ -143,11 +149,15 @@ function removeAcento(text) {
     return text;                 
 }
 
+
+// Englobar isso em uma IIFE?
 let livrosRestantes = []
 for (let livro of document.getElementsByClassName('btn-check')) {
     livrosRestantes.push(livro.id)
 }
 
+
+// Englobar isso em uma IIFE?²
 let inputLivro = document.getElementById('inputLivro')
 if (inputLivro) {
     inputLivro.addEventListener('keyup', function() {
